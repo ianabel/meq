c1706e946fafdd0f043aa428fcfdabf6d88446b2
Ian Abel <ian@utumno.org.uk>
2019-09-18 18:23:01 -0400

    synchronise

--- Refiners.cpp
+++ Refiners.cpp
@@ -1,5 +1,13 @@
 
+
 #include "Refiners.hpp"
+
+#include <limits>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <iterator>
+
 
 namespace mfem {
 
@@ -19,7 +27,6 @@ namespace mfem {
 
 	int MaximumMarkingRefiner::ApplyImpl(Mesh &mesh)
 	{
-		threshold = 0.0;
 		num_marked_elements = 0;
 		marked_elements.SetSize(0);
 		current_sequence = mesh.GetSequence();
@@ -51,9 +58,8 @@ namespace mfem {
 	/* Implementation from Pfeiler and Praetorius ( arXiv:1907.13078 ) 
 	 * with minor tweak to work with mfem::Vector as the original data structure.
 	 */
-	using Iterator_t = std::vector<double>::iterator;
 
-	const double xStarKernel( Iterator_t subX_begin, Iterator_t subX_end, double goal )
+	const double DoerflerMarkingRefiner::xStarKernel( Iterator_t subX_begin, Iterator_t subX_end, double goal )
 	{
 		// QuickMark , step ( i )-( ii ): partition by median element
 		auto length { std::distance( subX_begin , subX_end )};
@@ -70,14 +76,14 @@ namespace mfem {
 		return xStarKernel(++subX_middle, subX_end, goal - sigma_g - pivot_val );
 	}
 
-	const double compute_threshold ( const mfem::Vector & eta , double theta )
+	const double DoerflerMarkingRefiner::compute_threshold ( const mfem::Vector & eta , double theta )
 	{
 		std::vector<double> x { eta.GetData(), eta.GetData() + eta.Size() };
 		double goal = theta * std ::accumulate( x.cbegin(), x.cend(), ( double )0.0);
 		return xStarKernel( x.begin(), x.end(), goal);
 	} 
 
-	int DoerflerMarking::ApplyImpl(Mesh &mesh)
+	int DoerflerMarkingRefiner::ApplyImpl(Mesh &mesh)
 	{
 		threshold = 0.0;
 		num_marked_elements = 0;
@@ -91,7 +97,7 @@ namespace mfem {
 		const Vector &local_err = estimator.GetLocalErrors();
 		MFEM_ASSERT(local_err.Size() == NE, "invalid size of local_err");
 
-		double threshold = xStarKernel( local_err, gamma );
+		double threshold = compute_threshold( local_err, gamma );
 
 		for (int el = 0; el < NE; el++)
 		{
@@ -102,7 +108,6 @@ namespace mfem {
 		}
 
 		num_marked_elements = mesh.ReduceInt(marked_elements.Size());
-		MFEM_ASSERT( num_marked_elements == n, "Marking algorithm n is not the same as number of actually marked elements" );
 		if (num_marked_elements == 0) { return STOP; }
 
 		mesh.GeneralRefinement(marked_elements, non_conforming, nc_limit);
--- Refiners.hpp
+++ Refiners.hpp
@@ -1,149 +1,174 @@
 #ifndef REFINERS_HPP
 #define REFINERS_HPP
 
-#include "mfem.hpp"
+#include "../config/config.hpp"
+#include "../general/array.hpp"
+#include "mesh.hpp"
+#include "mesh_operators.hpp"
+#include "../fem/estimators.hpp"
+
+
 
 namespace mfem {
-/** @brief Mesh refinement using Maximum Marking
+	/** @brief Mesh refinement using Maximum Marking
 
-    This class uses the given ErrorEstimator to estimate local element errors
-    and then marks for refinement all elements i such that loc_err_i > threshold.
-    The threshold is computed as
-    \code
-       threshold = gamma * Max_{i in Mesh} { local_error_i }
-    \endcode
-	 where gamma is a configurable parameter
-*/
-class MaximumMarkingRefiner : public MeshOperator
-{
-protected:
-   ErrorEstimator &estimator;
+	  This class uses the given ErrorEstimator to estimate local element errors
+	  and then marks for refinement all elements i such that loc_err_i > threshold.
+	  The threshold is computed as
+	  \code
+	  threshold = gamma * Max_{i in Mesh} { local_error_i }
+	  \endcode
+	  where gamma is a configurable parameter
+	  */
+	class MaximumMarkingRefiner : public MeshOperator
+	{
+		protected:
+			ErrorEstimator &estimator;
 
-   long   max_elements;
+			long   max_elements;
 
-	double gamma;
-   double threshold;
-   long num_marked_elements;
+			double gamma;
+			double threshold;
+			long num_marked_elements;
 
-   Array<Refinement> marked_elements;
-   long current_sequence;
+			Array<Refinement> marked_elements;
+			long current_sequence;
 
-   int non_conforming;
-   int nc_limit;
+			int non_conforming;
+			int nc_limit;
 
-   /** @brief Apply the operator to the mesh.
-       @return STOP if a stopping criterion is satisfied or no elements were
-       marked for refinement; REFINED + CONTINUE otherwise. */
-   virtual int ApplyImpl(Mesh &mesh);
+			/** @brief Apply the operator to the mesh.
+			  @return STOP if a stopping criterion is satisfied or no elements were
+			  marked for refinement; REFINED + CONTINUE otherwise. */
+			virtual int ApplyImpl(Mesh &mesh) override;
 
-public:
-   /// Construct a ThresholdRefiner using the given ErrorEstimator.
-   MaximumMarkingRefiner(ErrorEstimator &est);
+		public:
+			/// Construct a MaximumMarkingRefiner using the given ErrorEstimator.
+			MaximumMarkingRefiner(ErrorEstimator &est);
 
-   // default destructor (virtual)
+			// default destructor (virtual)
 
-   void SetMaxElements(long max_elem) { max_elements = max_elem; }
+			void SetMaxElements(long max_elem) { max_elements = max_elem; }
 
-   /// Use nonconforming refinement, if possible (triangles, quads, hexes).
-   void PreferNonconformingRefinement() { non_conforming = 1; }
+			/// Use nonconforming refinement, if possible (triangles, quads, hexes).
+			void PreferNonconformingRefinement() { non_conforming = 1; }
 
-   /** @brief Use conforming refinement, if possible (triangles, tetrahedra)
-       -- this is the default. */
-   void PreferConformingRefinement() { non_conforming = -1; }
+			/** @brief Use conforming refinement, if possible (triangles, tetrahedra)
+			  -- this is the default. */
+			void PreferConformingRefinement() { non_conforming = -1; }
 
-   /** @brief Set the maximum ratio of refinement levels of adjacent elements
-       (0 = unlimited). */
-   void SetNCLimit(int nc_limit)
-   {
-      MFEM_ASSERT(nc_limit >= 0, "Invalid NC limit");
-      this->nc_limit = nc_limit;
-   }
+			/** @brief Set the maximum ratio of refinement levels of adjacent elements
+			  (0 = unlimited). */
+			void SetNCLimit(int nc_limit)
+			{
+				MFEM_ASSERT(nc_limit >= 0, "Invalid NC limit");
+				this->nc_limit = nc_limit;
+			}
 
-   /// Get the number of marked elements in the last Apply() call.
-   long GetNumMarkedElements() const { return num_marked_elements; }
+			/// Get the number of marked elements in the last Apply() call.
+			long GetNumMarkedElements() const { return num_marked_elements; }
 
-   /// Get the threshold used in the last Apply() call.
-   double GetThreshold() const { return threshold; }
+			/// Set the confiugrable parameter gamma
+			void SetGamma( double theta ) { gamma = theta; }
+			/// Get the configured gamma
+			double GetGamma() const { return gamma; }
 
-   /// Reset the associated estimator.
-	virtual void Reset() {
-		estimator.Reset();
-		current_sequence = -1;
-		num_marked_elements = 0;
+			/// Get the threshold used in the last Apply() call.
+			double GetThreshold() const { return threshold; }
+
+			/// Reset the associated estimator.
+			virtual void Reset() override {
+				estimator.Reset();
+				current_sequence = -1;
+				num_marked_elements = 0;
+			};
 	};
-};
 
-/** @brief Mesh refinement using Dörfler Marking
+	/** @brief Mesh refinement using Dörfler Marking
 
-    This class uses the given ErrorEstimator to estimate local element errors
-    and then marks for refinement a set M such that
-	 \code
-       gamma Sum_{i in M} local_error_i^2 <= Sum_{i in Mesh} local_error_i^2
-    \endcode
-	 where gamma is a configurable parameter.
-	 Currently we use the trivial O( N log( N ) ) algorithm to compute the set M 
-	 (N is the number of elements in the mesh ), but initial code for the linear 
-	 complexity QuickMark algorithm of Pfeiler and Praetorius ( arXiv:1907.13078 ) 
-	 is present.
-*/
-class DoerflerMarkingRefiner : public MeshOperator
-{
-protected:
-   ErrorEstimator &estimator;
+	  This class uses the given ErrorEstimator to estimate local element errors
+	  and then marks for refinement a set M such that
+	  \code
+	  gamma Sum_{i in M} local_error_i^2 <= Sum_{i in Mesh} local_error_i^2
+	  \endcode
+	  where gamma is a configurable parameter.
+	  Currently we use the linear complexity QuickMark algorithm of Pfeiler and Praetorius ( arXiv:1907.13078 ).
+	  */
+	class DoerflerMarkingRefiner : public MeshOperator
+	{
+		public:
+			using Iterator_t = std::vector<double>::iterator;
+		protected:
+			ErrorEstimator &estimator;
 
-   long   max_elements;
+			long   max_elements;
 
-   double threshold;
-	double gamma;
-   long num_marked_elements;
+			double threshold;
+			double gamma;
+			long num_marked_elements;
 
-   Array<Refinement> marked_elements;
-   long current_sequence;
+			Array<Refinement> marked_elements;
+			long current_sequence;
 
-   int non_conforming;
-   int nc_limit;
+			int non_conforming;
+			int nc_limit;
 
-   /** @brief Apply the operator to the mesh.
-       @return STOP if a stopping criterion is satisfied or no elements were
-       marked for refinement; REFINED + CONTINUE otherwise. */
-   virtual int ApplyImpl(Mesh &mesh);
 
-public:
-   /// Construct a ThresholdRefiner using the given ErrorEstimator.
-   DoerflerMarkingRefiner(ErrorEstimator &est);
+			/** @brief Compute the threshold using the linear complexity QuickMark algorithm of Pfeiler & Praetorius ( arXiv:1907.13078 )
+			 * @return Threshold x* such that the Dörfler set is precisely the set err > x*
+			 */
+			const double compute_threshold ( const mfem::Vector & eta , double theta );
+			const double xStarKernel( Iterator_t subX_begin, Iterator_t subX_end, double goal );
 
-   // default destructor (virtual)
 
-   void SetMaxElements(long max_elem) { max_elements = max_elem; }
+			/** @brief Apply the operator to the mesh.
+			  @return STOP if a stopping criterion is satisfied or no elements were
+			  marked for refinement; REFINED + CONTINUE otherwise. */
+			virtual int ApplyImpl(Mesh &mesh) override;
 
-   /// Use nonconforming refinement, if possible (triangles, quads, hexes).
-   void PreferNonconformingRefinement() { non_conforming = 1; }
+		public:
+			/// Construct a ThresholdRefiner using the given ErrorEstimator.
+			DoerflerMarkingRefiner(ErrorEstimator &est);
 
-   /** @brief Use conforming refinement, if possible (triangles, tetrahedra)
-       -- this is the default. */
-   void PreferConformingRefinement() { non_conforming = -1; }
+			// default destructor (virtual)
 
-   /** @brief Set the maximum ratio of refinement levels of adjacent elements
-       (0 = unlimited). */
-   void SetNCLimit(int nc_limit)
-   {
-      MFEM_ASSERT(nc_limit >= 0, "Invalid NC limit");
-      this->nc_limit = nc_limit;
-   }
+			void SetMaxElements(long max_elem) { max_elements = max_elem; }
 
-   /// Get the number of marked elements in the last Apply() call.
-   long GetNumMarkedElements() const { return num_marked_elements; }
+			/// Use nonconforming refinement, if possible (triangles, quads, hexes).
+			void PreferNonconformingRefinement() { non_conforming = 1; }
 
-   /// Get the threshold used in the last Apply() call.
-   double GetThreshold() const { return threshold; }
+			/** @brief Use conforming refinement, if possible (triangles, tetrahedra)
+			  -- this is the default. */
+			void PreferConformingRefinement() { non_conforming = -1; }
 
-   /// Reset the associated estimator.
-	virtual void Reset() {
-		estimator.Reset();
-		current_sequence = -1;
-		num_marked_elements = 0;
+			/** @brief Set the maximum ratio of refinement levels of adjacent elements
+			  (0 = unlimited). */
+			void SetNCLimit(int nc_limit)
+			{
+				MFEM_ASSERT(nc_limit >= 0, "Invalid NC limit");
+				this->nc_limit = nc_limit;
+			}
+
+			/// Get the number of marked elements in the last Apply() call.
+			long GetNumMarkedElements() const { return num_marked_elements; }
+
+			/// Set the confiugrable parameter gamma
+			void SetGamma( double theta ) { gamma = theta; }
+			/// Get the configured gamma
+			double GetGamma() const { return gamma; }
+
+
+
+			/// Get the threshold used in the last Apply() call.
+			double GetThreshold() const { return threshold; }
+
+			/// Reset the associated estimator.
+			virtual void Reset() override {
+				estimator.Reset();
+				current_sequence = -1;
+				num_marked_elements = 0;
+			};
 	};
-}
 
 }
 #endif // REFINERS_HPP
