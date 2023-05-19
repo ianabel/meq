c6bb7340135bc567f408d2e8367231ed911d6387
Ian Abel <ian@utumno.org.uk>
2023-05-18 20:50:53 -0400

    late night hacking

--- tools/plasma-output/ReadEquilibrium.cpp
+++ tools/plasma-output/ReadEquilibrium.cpp
@@ -5,4 +5,9 @@
 
 Equilbrium::Equilibrium( std::string const& netcdf_file )
 {
+	netCDF::NcFile data_file;
+	data_file.open( netcdf_file, netCDF::NcFile::FileMode::read );
+	netCDF::NcDim R_dim,Z_dim;
+	R_dim = data_file.getDim( "R" );
+	Z_dim = data_file.getDim( "Z" );
 }
